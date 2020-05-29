3771323573fa907391c1cc97bbac1eed8396e137
Michael Gregory <michael.gregory8584@gmail.com>
2020-05-28 19:40:18 -0500

    mvp reached

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 // Contexts
-import {ProductContext} from './contexts/ProductContext'
+import { ProductContext } from './contexts/ProductContext'
+import { CartContext } from './contexts/CartContext'
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -23,16 +24,17 @@ function App() {
 		<div className="App"> 
 			<ProductContext.Provider value={{products, addItem}}>
 				{/* why the double curly here? ask in review! */}
-				<Navigation cart={cart} />
+				<CartContext.Provider value={cart} >					
+					<Navigation />
+					{/* Routes */}
+					<Route exact path="/" >
+						<Products />
+					</Route>
 
-				{/* Routes */}
-				<Route exact path="/">
-					<Products />
-				</Route>
-
-				<Route path="/cart">
-					<ShoppingCart cart={cart} />
-				</Route>
+					<Route path="/cart" >
+						<ShoppingCart />
+					</Route>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
